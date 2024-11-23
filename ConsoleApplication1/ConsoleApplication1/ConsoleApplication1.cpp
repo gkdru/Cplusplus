ba86aff37f252b25397f3cbd7bc4b8bc211f3bae
unknown <amirkaragerei@gmail.com>
2024-11-23 13:59:05 +0600

    C++ HW #6

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,128 +1,109 @@
 ﻿#include <iostream>
-#include <cmath>
 
 using namespace std;
 
-class Fraction {
-    int gcd(int a, int b) {
-        int res = min(a, b);
-        while (res > 1) {
-            if (a % res == 0 && b % res == 0)
-                break;
-            res--;
-        }
-        return res;
-    }
-
-
-    int numerator;
-    int denominator;
-
-    void reduce() {
-        int nod = gcd(numerator, denominator);
-        numerator /= nod;
-        denominator /= nod;
-        if (denominator < 0) {
-            numerator = -numerator;
-            denominator = -denominator;
-        }
-    }
+class Overcoat {
+    string type;
+    double price;
 
 public:
-    Fraction(int num = 0, int den = 1) : numerator(num), denominator(den) {
-        if (den == 0) {
-            throw invalid_argument("Denominator cannot be zero!");
+    Overcoat(string t = "Unknown", double p = 0.0) : type(t), price(p) {}
+
+    bool operator==(const Overcoat& other) const {
+        return this->type == other.type;
+    }
+
+    Overcoat& operator=(const Overcoat& other) {
+        if (this != &other) {
+            type = other.type;
+            price = other.price;
         }
-        reduce();
+        return *this;
     }
 
-    Fraction operator+(const Fraction& other) const {
-        return Fraction(numerator * other.denominator + other.numerator * denominator,
-            denominator * other.denominator);
+    bool operator>(const Overcoat& other) const {
+        return this->price > other.price;
     }
 
-    Fraction operator-(const Fraction& other) const {
-        return Fraction(numerator * other.denominator - other.numerator * denominator,
-            denominator * other.denominator);
-    }
-
-    Fraction operator*(const Fraction& other) const {
-        return Fraction(numerator * other.numerator, denominator * other.denominator);
-    }
-
-    Fraction operator/(const Fraction& other) const {
-        if (other.numerator == 0) {
-            throw invalid_argument("Division by zero!");
-        }
-        return Fraction(numerator * other.denominator, denominator * other.numerator);
-    }
-
-    friend ostream& operator<<(ostream& os, const Fraction& f) {
-        os << f.numerator << "/" << f.denominator;
-        return os;
+ 
+    void display() const {
+        cout << "Type: " << type << ", Price: " << price << endl;
     }
 };
 
-class Complex {
-    double real;
-    double imag;
+class Flat {
+private:
+    double area;
+    double price;
 
 public:
-    Complex(double r = 0.0, double i = 0.0) : real(r), imag(i) {}
+    Flat(double a = 0.0, double p = 0.0) : area(a), price(p) {}
 
-    Complex operator+(const Complex& other) const {
-        return Complex(real + other.real, imag + other.imag);
+    bool operator==(const Flat& other) const {
+        return this->area == other.area;
     }
-
-    Complex operator-(const Complex& other) const {
-        return Complex(real - other.real, imag - other.imag);
-    }
-
-    Complex operator*(const Complex& other) const {
-        return Complex(real * other.real - imag * other.imag, real * other.imag + imag * other.real);
-    }
-
-    Complex operator/(const Complex& other) const {
-        double denominator = other.real * other.real + other.imag * other.imag;
-        if (denominator == 0) {
-            throw invalid_argument("Division by zero!");
+    Flat& operator=(const Flat& other) {
+        if (this != &other) {
+            area = other.area;
+            price = other.price;
         }
-        return Complex((real * other.real + imag * other.imag) / denominator,
-            (imag * other.real - real * other.imag) / denominator);
+        return *this;
+    }
+    bool operator>(const Flat& other) const {
+        return this->price > other.price;
     }
 
-
-    friend ostream& operator<<(ostream& os, const Complex& c) {
-        os << c.real;
-        if (c.imag >= 0) {
-            os << " + " << c.imag << "i";
-        }
-        else {
-            os << " - " << -c.imag << "i";
-        }
-        return os;
+    void display() const {
+        cout << "Area: " << area << " sqm, Price: " << price << " USD" << endl;
     }
 };
+
+
+
 
 int main() {
+    Overcoat coat1("Wool Coat", 150.0);
+    Overcoat coat2("Wool Coat", 120.0);
+    Overcoat coat3("Leather Coat", 200.0);
 
-        Fraction f1(3, 4);
-        Fraction f2(5, 6);
+    if (coat1 == coat2) {
+        cout << "Coat1 and Coat2 are of the same type." << endl;
+    }
+    else {
+        cout << "Coat1 and Coat2 are of different types." << endl;
+    }
 
-        cout << "f1 = " << f1 << ", f2 = " << f2 << endl;
-        cout << "f1 + f2 = " << f1 + f2 << endl;
-        cout << "f1 - f2 = " << f1 - f2 << endl;
-        cout << "f1 * f2 = " << f1 * f2 << endl;
-        cout << "f1 / f2 = " << f1 / f2 << endl;
+    coat3 = coat1;
+    coat3.display();
 
-        Complex c1(3, 4);
-        Complex c2(1, -2);
+    if (coat1 > coat2) {
+        cout << "Coat1 is more expensive than Coat2." << endl;
+    }
+    else {
+        cout << "Coat1 is cheaper than or equal to Coat2." << endl;
+    }
 
-        cout << "c1 = " << c1 << ", c2 = " << c2 << endl;
-        cout << "c1 + c2 = " << c1 + c2 << endl;
-        cout << "c1 - c2 = " << c1 - c2 << endl;
-        cout << "c1 * c2 = " << c1 * c2 << endl;
-        cout << "c1 / c2 = " << c1 / c2 << endl;
+    Flat flat1(50.0, 100000);
+    Flat flat2(50.0, 95000);
+    Flat flat3(70.0, 120000.100);
+
+    if (flat1 == flat2) {
+        cout << "Flat1 and Flat2 have the same area." << endl;
+    }
+    else {
+        cout << "Flat1 and Flat2 have different areas." << endl;
+    }
+
+    flat3 = flat1;
+    flat3.display();
+
+    if (flat1 > flat2) {
+        cout << "Flat1 is more expensive than Flat2." << endl;
+    }
+    else {
+        cout << "Flat1 is cheaper than or equal to Flat2." << endl;
+    }
+
 
 
     return 0;
